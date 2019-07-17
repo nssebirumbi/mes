98684713b6ad824492d9ed4e117122fff483eecd
microsoftjulius <julisema4@gmail.com>
2019-07-16 22:28:17 -0700

    Edited Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutternestedtabsapp/moviereleasetabs.dart';
 import 'news.dart';
 
 void main() => runApp(MyApp());
 const PrimaryColor = const Color(0xFF151026);
-
+ //Creating routes
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter NestedTabs Demo',
+      title: 'Mes Application',
       theme: ThemeData(
         primaryColor: Color(0xffffffff),
       ),
       home: HomePage(),
+      routes: <String, WidgetBuilder>{
+        "/courses": (BuildContext context) => new MovieReleaseTabs(),
+      },
     );
   }
 }
